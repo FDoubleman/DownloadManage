e25fab2154601f296901ee523e1af299d8557bc3
FDoubleMan <fmm_330@163.com>
2017-12-19 11:11:20 +0800

    1、修改数据库表

--- app/src/main/java/cn/wdcloud/download/downloadUtils/db/DBInterface.java
+++ app/src/main/java/cn/wdcloud/download/downloadUtils/db/DBInterface.java
@@ -1,10 +1,142 @@
 package cn.wdcloud.download.downloadUtils.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import org.greenrobot.greendao.query.DeleteQuery;
+import org.greenrobot.greendao.query.Query;
+
+import java.util.List;
+
+import cn.wdcloud.download.MyApp;
+
 /**
  * Created by fmm on 2017/12/19.
  */
 
 public class DBInterface {
+    private Context mContext;
+    private static DBInterface instance;
+    private String DEFULT_DB_NAME ="mydefult.db";
+    private DaoMaster.DevOpenHelper openHelper;
 
+    private static final int STATUS_SUCCESS = 1;//成功
+    private static final int STATUS_FAILURE = 0;//失败
+    private static final int STATUS_RUNNING = 2;//下载中
+    private static final int STATUS_WAITING = 3;//等待
+    private static final int STATUS_PAUSE = 4;//暂停下载
+    private static final int STATUS_EXCAPTION = -1;//异常
+
+    public static DBInterface getInstance(){
+        if(instance ==null){
+            synchronized (DBInterface.class){
+                if(instance ==null){
+                    instance =new DBInterface();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+    public DBInterface(){
+        mContext = MyApp.sContext;
+    }
+
+    //初始化数据库
+    public void initDBHelp() {
+        reset();
+        Log.d("initDBHelp", "打开数据库");
+        openHelper = new DaoMaster.DevOpenHelper(mContext, DEFULT_DB_NAME, null);
+    }
+
+    private void reset() {
+        if (openHelper != null) {
+            Log.d("reset", "关闭数据库");
+            openHelper.close();
+            openHelper = null;
+        }
+    }
+
+    private void isInitOk() {
+        if (openHelper == null) {
+            Log.e("isInitOk", "openHelper未初始化");
+            openHelper = new DaoMaster.DevOpenHelper(mContext, DEFULT_DB_NAME, null);
+            Log.e("isInitOk", "openHelper重新初始化");
+        }
+    }
+
+    private DaoSession openWritableDb() {
+        isInitOk();
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        DaoMaster master = new DaoMaster(db);
+        return master.newSession();
+    }
+
+    private DaoSession openReadAbleDb() {
+        isInitOk();
+        SQLiteDatabase db = openHelper.getReadableDatabase();
+        DaoMaster master = new DaoMaster(db);
+        return master.newSession();
+    }
+
+    /**
+     * 增 修改
+     */
+    public long insertOrUpdate(DownloadBean downloadBean) {
+        DownloadBeanDao dao = openWritableDb().getDownloadBeanDao();
+        return dao.insertOrReplace(downloadBean);
+    }
+
+    /**
+     * 删全部
+     */
+    public void deleteAll() {
+        DownloadBeanDao dao = openWritableDb().getDownloadBeanDao();
+        dao.deleteAll();
+    }
+
+    /**
+     * 根据Url 删除数据
+     */
+    public void deleteByUrl(String url){
+        DownloadBeanDao dao = openWritableDb().getDownloadBeanDao();
+        DeleteQuery<DownloadBean> db =dao.queryBuilder()
+                .where(DownloadBeanDao.Properties.Url.eq(url))
+                .buildDelete();
+        db.executeDeleteWithoutDetachingEntities();
+    }
+
+    /**
+     * 查询全部
+     */
+    public List<DownloadBean> qureAll(){
+        DownloadBeanDao dao = openReadAbleDb().getDownloadBeanDao();
+
+        return dao.loadAll();
+    }
+
+    /**
+     * 查询全部 已下载
+     */
+    public List<DownloadBean> qureAllDownloaded(){
+        DownloadBeanDao dao = openReadAbleDb().getDownloadBeanDao();
+        Query<DownloadBean> query =dao.queryBuilder()
+                .where(DownloadBeanDao.Properties.Status.eq(STATUS_SUCCESS))
+                .build();
+        return query.list();
+    }
+
+    /**
+     * 查询全部 正在下载
+     */
+    public List<DownloadBean> qureAllDownloading(){
+        DownloadBeanDao dao = openReadAbleDb().getDownloadBeanDao();
+        Query<DownloadBean> query =dao.queryBuilder()
+                .where(DownloadBeanDao.Properties.Status.notEq(STATUS_SUCCESS))
+                .build();
+        return query.list();
+    }
 
 }
--- app/src/main/java/cn/wdcloud/download/downloadUtils/db/DownloadBean.java
+++ app/src/main/java/cn/wdcloud/download/downloadUtils/db/DownloadBean.java
@@ -12,15 +12,18 @@ public class DownloadBean {
     @Id(autoincrement = true)
     private long id;
     private String url;//下载地址
-    private long totalSize;//下载大小
-    private long currentSize;//已下载大小
-    private int progress;//进度
-    private int status;//下载状态
-    private String filePath;//文件路径
+    private long totalSize =0L;//下载大小
+    private long currentSize=0L;//已下载大小
+    private int progress=0;//进度
+    private int status ;//下载状态
+    private String filePath;//本地文件路径
     private String fileName;//文件名称
-    @Generated(hash = 996458262)
+    private String startTime;//开始时间
+
+    @Generated(hash = 1148040777)
     public DownloadBean(long id, String url, long totalSize, long currentSize,
-            int progress, int status, String filePath, String fileName) {
+            int progress, int status, String filePath, String fileName,
+            String startTime) {
         this.id = id;
         this.url = url;
         this.totalSize = totalSize;
@@ -29,6 +32,7 @@ public class DownloadBean {
         this.status = status;
         this.filePath = filePath;
         this.fileName = fileName;
+        this.startTime = startTime;
     }
     @Generated(hash = 2040406903)
     public DownloadBean() {
@@ -80,6 +84,12 @@ public class DownloadBean {
     }
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+    public String getStartTime() {
+        return this.startTime;
+    }
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
     }
     
 }
